a7f493de9d36314ed130bba3dc28580e19a5312f
Carlos Lopes <clfreitaas18@gmail.com>
2026-05-07 15:27:31 -0400

    2º commit - ADICIONANDO WHILE E IF/ELIF/ELSE NO MENU.py

--- menu.py
+++ menu.py
@@ -3,10 +3,22 @@
 user = "carlos"
 opc = 1
 
-while opc == 1:
-    print(f"--- BEM VINDO {user.upper()} ---")
-    print("----- MENU DO ESTOQUE -----"
-      "1 - CADASTRAR PRODUTOS"
-      "2 - CONSULTAR PRODUTOS CADASTRADOS" \
-      "3 - EDITAR PRODUTOS" \
+while opc != 0:
+    print(f"--- BEM VINDO {user.upper()} ---\n")
+    print("----- MENU DO ESTOQUE -----\n"
+      "1 - CADASTRAR PRODUTOS\n"
+      "2 - CONSULTAR PRODUTOS CADASTRADOS\n" 
+      "3 - EDITAR PRODUTOS\n"
       "0 - SAIR")
+    opc = int(input(""))
+
+    if opc == 1:
+        print("")
+    elif opc == 2:
+        print("")
+    elif opc == 3:
+        print("")
+    elif opc == 0:
+        print("")
+    else:
+        print("OPÇÃO INEXISTENTE! DIGITE NOVAMENTE")
