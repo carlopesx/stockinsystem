79ba746a4b56c31d891b88aa5c24b12c419b6166
Carlos Lopes <clfreitaas18@gmail.com>
2026-05-07 13:12:37 -0400

    1º commit - CRIANDO MENU

---
+++ menu.py
@@ -0,0 +1,12 @@
+#Menu onde irá ocorrer a navegação do sistema
+
+user = "carlos"
+opc = 1
+
+while opc == 1:
+    print(f"--- BEM VINDO {user.upper()} ---")
+    print("----- MENU DO ESTOQUE -----"
+      "1 - CADASTRAR PRODUTOS"
+      "2 - CONSULTAR PRODUTOS CADASTRADOS" \
+      "3 - EDITAR PRODUTOS" \
+      "0 - SAIR")
